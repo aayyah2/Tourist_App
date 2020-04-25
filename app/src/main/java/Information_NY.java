41a35f1ac818033c26052206257f22879443a048
aparnaayyah <aayyah2@illinois.edu>
2020-04-25 15:16:44 -0500

    Initial commit

---
+++ app/src/main/java/Information_NY.java
@@ -0,0 +1,22 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class Information_NY {
+    private JSONObject nyInfo = new JSONObject();
+    public Information_NY() {
+       //nyInfo.put("state", "New York");
+       JSONArray arr = new JSONArray();
+       JSONObject p1 = new JSONObject();
+       //p1.put("name", "Empire State Building");
+       //arr.add(p1);
+       //arr.put(0, p1);
+
+
+
+
+    }
+
+
+
+}
